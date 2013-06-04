4669c4bb19e0f726211fbff704756f03c38eccf9
Vincenzo Maffione <v.maffione@gmail.com>
2013-06-04 15:37:21 +0200

    paravirt: qemu-v1.5.0 patch generated

--- patch.sh
+++ patch.sh
@@ -8,7 +8,7 @@ STARTCOMMIT="96fbd7de3656583d647c204c"
 ENDCOMMIT="paravirt"
 OUTPUT=patch.diff
 
-EXCLUDED_FILES="e1000-paravirt-README"
+EXCLUDED_FILES="e1000-paravirt-README patch.sh hw\/dma\/pl330 util\/hexdump.c util\/iov.c qemu-v1.5.0.diff.txt"
 PATCHED_FILES=$(git diff --stat $STARTCOMMIT $ENDCOMMIT | awk '{print $1}' | sed '$d' |  sed '/BSD/d' | sed '/virtio/d' | tr "\n" " ")
 
 # remove excluded files from "PATCHED_FILES"
