169d96773fe71c534f6eb7c39151866e5c7ca841
Vincenzo Maffione <v.maffione@gmail.com>
2013-10-30 17:31:35 +0100

    helpful configure script added

---
+++ myconfigure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#set -x
+
+NETMAP="--enable-netmap --extra-cflags=-I/home/vmaffione/git/lettieri/release/sys"
+E1000PARA="--enable-e1000-paravirt"
+
+./configure --target-list=x86_64-softmmu --enable-kvm --enable-vhost-net --python=python2 --disable-werror --enable-debug ${NETMAP} ${E1000PARA}
