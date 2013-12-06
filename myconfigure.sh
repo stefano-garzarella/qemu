02b32cadd64958f9f945e8f2d3649cfb160db082
Vincenzo Maffione <v.maffione@gmail.com>
2013-12-06 12:23:37 +0100

    [myconfigure] -O2 option

--- myconfigure.sh
+++ myconfigure.sh
@@ -5,4 +5,4 @@
 NETMAP="--enable-netmap --extra-cflags=-I/home/vmaffione/git/lettieri/netmap-release/sys"
 E1000PARA="--enable-e1000-paravirt"
 
-./configure --target-list=x86_64-softmmu --enable-kvm --enable-vhost-net --python=python2 --disable-werror --enable-debug ${NETMAP} ${E1000PARA}
+CFLAGS="-g -O2" ./configure --target-list=x86_64-softmmu --enable-kvm --enable-vhost-net --python=python2 --disable-werror --enable-debug ${NETMAP} ${E1000PARA}
